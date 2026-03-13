881dea86640af8eeef7088494ebc930c1fe11f90
Pratik Singh <pratikthakur4761@gmail.com>
2026-03-13 13:59:19 +0530

    Initial Commit.

---
+++ src/services/movieApi.ts
@@ -0,0 +1,7 @@
+import type { Movie } from "../types/movie";
+async function getTrendingMovies(): Promise<Movie[]> {
+
+
+}
+
+export default getTrendingMovies;
---
+++ src/types/movie.ts
@@ -0,0 +1,10 @@
+type Movie = {
+  id: number,
+  title: string,
+  poster_path: string,
+  vote_average: number,
+  release_date: string,
+  overview: string,
+
+}
+export type { Movie }
